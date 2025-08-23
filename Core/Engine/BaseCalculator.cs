5deca7f42936e07b240e2b47d035fbc21c88c323
이소현 <sohyun@ati2000.co.kr>
2025-08-23 18:03:38 +0900

    refactor: Move arithmetic operations from CalcValue to BaseCalculator
    연산자를 옮겨 클래스 단일 책임화 명확하게 함

--- Core/Engine/BaseCalculator.cs
+++ Core/Engine/BaseCalculator.cs
@@ -1,4 +1,5 @@
-﻿using Calculator.Core.Models;
+using System;
+using Calculator.Core.Models;
 
 namespace Calculator.Core.Engine
 {
@@ -6,11 +7,59 @@ namespace Calculator.Core.Engine
     {
         protected readonly Parser parser;
         protected readonly Evaluator evaluator;
+
         protected BaseCalculator()
         {
             parser = new Parser();
             evaluator = new Evaluator();
         }
+
+        // 공통 기본 연산들
+        public virtual CalcValue Add(CalcValue left, CalcValue right)
+        {
+            if (left == null || right == null) throw new ArgumentNullException();
+            return new CalcValue(left.Value + right.Value);
+        }
+
+        public virtual CalcValue Subtract(CalcValue left, CalcValue right)
+        {
+            if (left == null || right == null) throw new ArgumentNullException();
+            return new CalcValue(left.Value - right.Value);
+        }
+
+        public virtual CalcValue Multiply(CalcValue left, CalcValue right)
+        {
+            if (left == null || right == null) throw new ArgumentNullException();
+            return new CalcValue(left.Value * right.Value);
+        }
+
+        public virtual CalcValue Divide(CalcValue left, CalcValue right)
+        {
+            if (left == null || right == null) throw new ArgumentNullException();
+            if (right.Value == 0) throw new DivideByZeroException("0으로 나눌 수 없음");
+            return new CalcValue(left.Value / right.Value);
+        }
+
+        public virtual CalcValue Sqrt(CalcValue value)
+        {
+            if (value == null) throw new ArgumentNullException();
+            if (value.Value < 0) throw new InvalidOperationException("음수의 제곱근을 구할 수 없음");
+            return new CalcValue((decimal)Math.Sqrt((double)value.Value));
+        }
+
+        public virtual CalcValue Square(CalcValue value)
+        {
+            if (value == null) throw new ArgumentNullException();
+            return new CalcValue(value.Value * value.Value);
+        }
+
+        public virtual CalcValue Percent(CalcValue value)
+        {
+            if (value == null) throw new ArgumentNullException();
+            return new CalcValue(value.Value / 100);
+        }
+
+        // 추상 메서드 (표현식 평가)
         public abstract CalcValue Evaluate(string expression);
         public abstract bool ValidateExpression(string expression);
     }
