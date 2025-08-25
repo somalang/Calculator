14d465bc73e71b3f77ebb5d1aca9567f5a782fc8
somalang <sohyun@ati2000.co.kr>
2025-08-25 11:34:18 +0900

    feat: add HistoryWindow for viewing and managing calculation history

---
+++ UI/Views/HistoryWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Calculator.UI.ViewModels;
+
+namespace Calculator.UI.Views
+{
+    public partial class HistoryWindow : Window
+    {
+        public HistoryWindow(CalculatorViewModel calculatorViewModel)
+        {
+            InitializeComponent();
+            DataContext = new HistoryViewModel(calculatorViewModel, this);
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
