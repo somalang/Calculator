72eb090b90a57423582bfdedf29f3d84381da6aa
somalang <sohyun@ati2000.co.kr>
2025-08-25 13:24:53 +0900

    fix: resolve git stash pop conflicts and clean workspace for merge

--- obj/Debug/net8.0-windows/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cfe05a53ace6e2ac4b856e5dfa7b0b483b3355d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+570faa6e814a800241c873596c3463614c634e46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
